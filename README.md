0990309e43af7486b6b03f873d3efd6d0684ce72
Tim Jedrek <tim@timjedrek.com>
2021-12-26 01:23:09 -0600

    JS grid and mouseover

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Created 12-25-2021
 Tim Santos
 
+
 Visit preview: https://timjedrek.github.io/etch-a-sketch/
+
+Progress
+1. 12-25-2021 Tim Santos - Initial construction.  Completed frontend UI
+2. 12-26-2021 Tim Santos - Grid and mouse over function completed
--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let scale = 16;
-let i = 0; 
-let j = 0;
+let rows = 0; 
+let columns = 0;
 let width;
-let gridSize = 500
+let gridSize = 500 //500px is default width
+let colorSelection = '#2D232E'
+
 
 function grid(scale) {
-    const container = document.querySelector('#container');
+    //const container = document.querySelector('#container');
     calcWidth(scale);
 
     //Make div to hold the columns
@@ -17,32 +19,41 @@ function grid(scale) {
     //loop inside of a loop to make the grid
     
     //Make the columns
-    while (j<scale) {
+    while (columns<scale) {
         const column = document.createElement('div');
         column.setAttribute('id', 'column');
         column.style.width = width + "px";  
         columnHolder.appendChild(column);
 
         //make the cubes
-        while (i<scale) {
+        while (rows<scale) {
             const content = document.createElement('div');
-            content.classList.add(`cube`);
+            content.classList.add("cube");
+            content.setAttribute('id', `row${rows}column${columns}`);
             content.textContent = null;
             content.style.width = width + "px";  
             content.style.height = width + "px";
             column.appendChild(content);
-            i++; 
+            rows++; 
         }
-    i=0;
-    j++
+    rows=0;
+    columns++;
     }
+    columns=0;
     return;
 }
 
-//500 = height and width in pixels of the entire grid
 function calcWidth (scale) {
     width = gridSize/ scale;
     return width;
 }
 
+//start er up!
 grid(scale);
+
+//enable da hover-o-sensor thing-a-mabobbermajig
+document.querySelectorAll(".cube").forEach(item => {
+    item.addEventListener("mouseover", event => {
+        item.style.backgroundColor = colorSelection;
+    })
+})
